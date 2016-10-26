14f28ef08cc0160da2c88ea5ec1e44befae65908
Tom Bergeron <thbe7113@colorado.edu>
2016-10-26 12:47:47 -0600

    Update userdata.sql

--- makeDatabase/userdata.sql
+++ makeDatabase/userdata.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS 'users' (
-'username' VARCHAR(20) NOT NULL
-'firstname' VARCHAR(15) NOT NULL
-'lastname' VARCHAR(15) NOT NULL
-'email' VARCHAR(30)
-
-PRIMARY KEY ('username')
+CREATE TABLE IF NOT EXISTS 'userdata' (
+'myKey' int(20) NOT NULL
+'clientId' int(5)
+'groupKeys' int(5)
+'eventkey' int(5)
+  
 );
