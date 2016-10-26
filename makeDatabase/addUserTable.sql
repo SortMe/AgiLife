de0806845a74b3ceb09c8946ba5ea3f4a020aa03
Ethan Wright <ethanwright@rgnt2-109-99-dhcp.int.colorado.edu>
2016-10-26 13:14:45 -0600

    Added table?

---
+++ makeDatabase/addUserTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE userTable (
+myKey Int,      -- Key for table
+clientID Int,    -- Google Sign in Auth
+groupKeys Int,    --Key that links to group table (displays group ID's user belongs to)
+eventKey Int,     -- access event (dynamic or static)
+);
+
+
