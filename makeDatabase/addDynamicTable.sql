d555400185cf2552e22733c58a080b573e21ffc5
Ethan Wright <ethanwright@rgnt2-109-99-dhcp.int.colorado.edu>
2016-10-26 13:09:54 -0600

    Changed SQL stuff (table creation)

--- makeDatabase/addDynamicTable.sql
+++ makeDatabase/addDynamicTable.sql
@@ -1,11 +1,13 @@
 -- Create a table to store dynamic events, varchar allows for a variable amount
 CREATE TABLE dynamicEvent (
 myKey Int,
+eventKey Int,      -- Key associated with the userTable event
 event VARCHAR(100),
-weight Double,
+weight Double,    -- assigned by them
+timePreference Int, -- time of day preference
 timeStart DATETIME,  -- Output looks like: 2016-10-25 08:20:00
 timeEnd DATETIME, -- Optional, either enter length of event or completion date
 eventLength Double -- Optional, they can either enter length of event or time it takes to complete
 );
-
+-- start time, end time, period of time, total day preffered time of day 
 
--- makeDatabase/addStaticTable.sql
+++ makeDatabase/addStaticTable.sql
@@ -2,10 +2,11 @@
 
 CREATE TABLE staticEvent (
 myKey Int,
+eventKey Int, --Key associated with the userTable event
 event VARCHAR(100),
-required Boolean,
+-- required Boolean, --possible future implementation
 timeStart DATETIME, -- form looks like: 2016-10-25 08:20:00
-timeEnd DATETIME,
+timeEnd DATETIME, 
 eventLength Double -- Optional, either enter event length or time
 );
 
