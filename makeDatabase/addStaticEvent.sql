c927dc148a1375e4c7146dccd45780921bf6dfd4
Tom Bergeron <thbe7113@colorado.edu>
2016-10-27 11:38:59 -0600

    Update addStaticEvent.sql

--- makeDatabase/addStaticEvent.sql
+++ makeDatabase/addStaticEvent.sql
@@ -1,7 +1,7 @@
 -- Create a static event
-INSERT INTO 'staticEvent' ('myKey', 'eventKey', 'event', 'timeStart', 'timeEnd', 'eventLength')
-VALUES (1, 1, 'Study', 2016-10-25 08:20:00, 2016-10-25 11:20:00, 3.00);
-VALUES (1, 2, 'Study more', 2016-10-26 09:30:00, 2016-10-26 11:30:00, 2.00);
-VALUES (1, 3, 'Just keep studying buddy', 2016-10-27 08:00:00, 2016-10-27 12:30:00, 4.50);
-VALUES (1, 4, 'You are almost there!', 2016-10-28 15:00:00, 2016-10-28 20:20:00, 5.33);
-VALUES (1, 5, 'Finally the weekend, but you really should study more', 2016-10-29 08:00:00, 2016-10-29 20:00:00, 12.00);
+INSERT INTO 'staticEvent' ('myKey', 'eventKey', 'event', 'timeStart', 'timeEnd', 'eventLength') VALUES
+  (1, 1, 'Study', 2016-10-25 08:20:00, 2016-10-25 11:20:00, 3.00),
+  (1, 2, 'Study more', 2016-10-26 09:30:00, 2016-10-26 11:30:00, 2.00),
+  (1, 3, 'Just keep studying buddy', 2016-10-27 08:00:00, 2016-10-27 12:30:00, 4.50),
+  (1, 4, 'You are almost there!', 2016-10-28 15:00:00, 2016-10-28 20:20:00, 5.33),
+  (1, 5, 'Finally the weekend, but you really should study more', 2016-10-29 08:00:00, 2016-10-29 20:00:00, 12.00);
