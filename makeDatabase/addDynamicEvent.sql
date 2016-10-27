411ec16edb97bc0248e432dab5a1b8373c39428e
Tom Bergeron <thbe7113@colorado.edu>
2016-10-27 11:37:51 -0600

    Update addDynamicEvent.sql

--- makeDatabase/addDynamicEvent.sql
+++ makeDatabase/addDynamicEvent.sql
@@ -1,11 +1,7 @@
 -- Create a dynamic event, this is for testing purposes
-INSERT INTO 'dynamicEvent' ('myKey', 'eventKey', 'event', 'weight', 'timePreference', 'timeStart', 'timeEnd', 'eventLength')
-VALUES (1, 1, 'halloween jamz', 5.0, 2016-10-31 20:30:00, 2016-10-31 23:00:00, 150);
-VALUES (1, 2, 'breakfast jamz???', 3.2, 2016-11-03 08:00:00, 2016-11-03 12:00:00, 240);
-VALUES (2, 3, 'rando jamz', 1.1, 2016-10-29 12:12:00, 2016-10-29 15:15:00, 183);
-VALUES (3, 4, 'Stop it with the jamz', 10, 2016-10-27 11:15:00, 2016-10-27 11:16:00, 1);
-VALUES (3, 5, 'Okay no more jamz', 1.2, 2016-10-31 20:30:00, 2016-10-31 23:00:00, 150);
-
-
-
-
+INSERT INTO 'dynamicEvent' ('myKey', 'eventKey', 'event', 'weight', 'timePreference', 'timeStart', 'timeEnd', 'eventLength') VALUES
+  (1, 1, 'halloween jamz', 5.0, 2016-10-31 20:30:00, 2016-10-31 23:00:00, 150),
+  (1, 2, 'breakfast jamz???', 3.2, 2016-11-03 08:00:00, 2016-11-03 12:00:00, 240),
+  (2, 3, 'rando jamz', 1.1, 2016-10-29 12:12:00, 2016-10-29 15:15:00, 183),
+  (3, 4, 'Stop it with the jamz', 10, 2016-10-27 11:15:00, 2016-10-27 11:16:00, 1),
+  (3, 5, 'Okay no more jamz', 1.2, 2016-10-31 20:30:00, 2016-10-31 23:00:00, 150);
