815fb4dea13e22470ff92d52cd4ab13ad37babcb
Tom Bergeron <thbe7113@colorado.edu>
2016-10-27 11:18:48 -0600

    Update addStaticTable.sql

--- makeDatabase/addStaticTable.sql
+++ makeDatabase/addStaticTable.sql
@@ -1,13 +1,13 @@
 -- Create static events in database
 
-CREATE TABLE staticEvent (
-myKey Int,
-eventKey Int, --Key associated with the userTable event
-event VARCHAR(100),
+CREATE TABLE IF NOT EXSISTS 'staticEvent' (
+'myKey' Int,
+'eventKey' Int, --Key associated with the userTable event
+'event' VARCHAR(100),
 -- required Boolean, --possible future implementation
-timeStart DATETIME, -- form looks like: 2016-10-25 08:20:00
-timeEnd DATETIME, 
-eventLength Double -- Optional, either enter event length or time
+'timeStart' DATETIME, -- form looks like: 2016-10-25 08:20:00
+'timeEnd' DATETIME, 
+'eventLength' Double -- Optional, either enter event length or time
 );
 
 
