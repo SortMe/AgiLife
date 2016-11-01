6eefdd79d91b0e7a8b5e62452f1ce030037b66a2
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2016-11-01 08:39:52 -0600

    refactored for single lists of tuples instead of two separate lists, cut down some of the code to make more clear

--- algorithm/staticEvent.py
+++ algorithm/staticEvent.py
@@ -16,98 +16,90 @@
 #   - checks for proper category to be placed in
 #   - checks to make sure it wouldn't overlap with another event
 
-#  checkValid: params(a list of durations to check, a list of start times that
+#  check_valid: params(a list of durations to check, a list of start times that
 #  correlate to durations, and the start time of the function to be added)
 #    The helper function to check for overlapping events 
 
 
 class Event(object):    #Create class for managing Event Time
   def __init__(self, startTime, length):
-#Parameters
-    self.startTime = []
-    self.length = []
+
+#Establish lists for Time, these will be tuples of (startTime, length)
+    self.early_morning_time = []
+    self.late_morning_time = []
+    self.early_evening_time = []
+    self.late_evening_time = []
+
+  def add_late_morning(self, startTime, length):
+    self.late_morning_time.append((startTime,length))
 
 
-#Establish lists for Time
-    self.EarlyMorningTime = []
-    self.LateMorningTime = []
-    self.EarlyEveningTime = []
-    self.LateEveningTime = []
+  def add_early_morning(self, startTime, length):
+    self.early_morning_time.append((startTime,length))
 
-#Establish lists for duration
-    self.EarlyMorningLength = []
-    self.LateMorningLength = []
-    self.EarlyEveningLength = []
-    self.LateEveningLength = []
 
-  def addLateMorning(self, startTime, length):
-    self.LateMorningTime.append(startTime)
-    self.LateMorningLength.append(length)
+  def add_late_evening(self, startTime, length):
+    self.late_evening_time.append((startTime,length))
 
-  def addEarlyMorning(self, startTime, length):
-    self.EarlyMorningTime.append(startTime)
-    self.EarlyMorningLength.append(length)
 
-  def addLateEvening(self, startTime, length):
-    self.LateEveningTime.append(startTime)
-    self.LateEveningLength.append(length)
+  def add_early_evening(self, startTime, length):
+    self.early_evening_time.append((startTime, length))
 
-  def addEarlyEvening(self, startTime, length):
-    self.EarlyEveningTime.append(startTime)
-    self.EarlyEveningLength.append(length)
               
 #Function checks to make sure the added value doesn't conflict with existing events
-  def checkValid(self, listLength, listTime, checkTime, checkLength):  # list, list, int
-    for i in range(0, len(listLength)):
-      timeBetweenEvents = abs(listTime[i] - checkTime)
-      if timeBetweenEvents < listLength[i]:
+  def check_valid(self,list_time, check_time, check_length):  # list, list, int
+    for i in range(0, len(list_time)):
+      time_between_events = abs(list_time[i][0] - check_time)
+      if time_between_events < list_time[i][1]:
         return False
-      if timeBetweenEvents < checkLength:
+      if time_between_events < check_length:
         return False
 
     return True
 
 #Function called to input new events
-  def buildEvent(self, time, length):
-    unitLength = 179
-    earlyMorning = 480
-    lateMorning = 660
-    earlyEvening = 840
-    lateEvening = 1000
+  def build_event(self, time, length):
+  #Minutes into day
+    unit_length = 179
+    early_morning = 480
+    late_morning = 660
+    early_evening = 840
+    late_evening = 1000
 
-    if time > earlyMorning and time < earlyMorning + unitLength:
-      if self.checkValid(self.EarlyMorningLength, self.EarlyMorningTime, time, length) == True:
-        self.addEarlyMorning(time, length)
+    if time > early_morning and time < early_morning + unit_length:
+      if self.check_valid(self.early_morning_time, time, length) == True:
+        self.add_early_morning(time, length)
 
-    if time > lateMorning and time < lateMorning + unitLength:
-      if self.checkValid(self.LateMorningLength, self.LateMorningTime, time, length) == True:
-        self.addLateMorning(time, length) 
+    if time > late_morning and time < lateMorning + unit_length:
+      if self.check_valid(self.late_morning_time, time, length) == True:
+        self.add_late_morning(time, length) 
 
-    if time > earlyEvening and time < earlyEvening + unitLength:
-      if self.checkValid(self.EarlyEveningLength, self.EarlyEveningTime, time, length) == True:
-        self.addEarlyEvening(time, length) 
+    if time > early_evening and time < earlyEvening + unit_length:
+      if self.check_valid(self.early_evening_time, time, length) == True:
+        self.add_early_evening(time, length) 
 
-    if time > lateEvening and time < lateEvening + unitLength:
-      if self.checkValid(self.LateEveningLength, self.LateEveningTime, time, length) == True:
-        self.addLateEvening(time, length) 
+    if time > late_evening and time < late_evening + unit_length:
+      if self.check_valid(self.late_evening_time, time, length) == True:
+        self.add_late_evening(time, length) 
 
 # Use Examples:
-# object.EarlyMorningTime --- print list of times for early morning events
+# object.early_morning_time --- print list of times for early morning events
 # object.EarlyMorningLength --print duration of the events
 # object.getEarlyMorning(30) --get all events that last for 30 minutes or
 # longer
 
 static = Event([],[])    #build object
-static.buildEvent(490, 5)  #add static events to object
-static.buildEvent(500, 10)  # params(start time, duration) ---> both in minutes
-static.buildEvent(565, 30)
-static.buildEvent(550, 40)
-static.buildEvent(540,30)
+static.build_event(490, 5)  #add static events to object
+static.build_event(500, 10)
+static.build_event(565, 30)
+static.build_event(550, 40)
+static.build_event(540,30)
 
 
 
 
-for i in range (0, len(static.EarlyMorningTime)):
-  print 'The event starts at: ', static.EarlyMorningTime[i]
-  print 'and lasts for: ',static.EarlyMorningLength[i] ,' minutes.'
+for i in range (0, len(static.early_morning_time)):
+  print 'The event starts at: ', round((static.early_morning_time[i][0]/60.0),2)
+  #Minutes into day
+  print 'and lasts for: ',static.early_morning_time[i][0] ,' minutes.'
 
