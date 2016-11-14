a4b97aca7bc1e7946bbaf360f84baf59dab1efb0
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2016-11-13 19:29:28 -0700

    fixed naming convention User1 -> event_object

--- algorithm/place_dynamic.py
+++ algorithm/place_dynamic.py
@@ -7,28 +7,28 @@ class Place_Event(object):
 
 
 
-    def calculate_event(self, preference, buffer, User1, dynamic_event_duration):
+    def calculate_event(self, preference, buffer, event_object, dynamic_event_duration):
         '''
-        options = {0 : User1.parse_list(User1.early_morning_time),
-            1 : User1.parse_list(User1.late_morning_time),
-            2 : User1.parse_list(User1.early_evening_time),
-            3 : User1.parse_list(User1.late_evening_time),
+        options = {0 : event_object.parse_list(event_object.early_morning_time),
+            1 : event_object.parse_list(event_object.late_morning_time),
+            2 : event_object.parse_list(event_object.early_evening_time),
+            3 : event_object.parse_list(event_object.late_evening_time),
             }
         options[preference]()
 '''
         '''  chose the correct list to work with  '''
         if (preference == 0):
-            User1.parse_list(User1.early_morning_time)
+            event_object.parse_list(event_object.early_morning_time)
         if (preference == 1):
-            User1.parse_list(User1.late_morning_time)
+            event_object.parse_list(event_object.late_morning_time)
         if(preference == 2):
-            User1.parse_list(User1.early_evening_time)
+            event_object.parse_list(event_object.early_evening_time)
         if(preference == 3):
-            User1.parse_list(User1.late_evening_time)
+            event_object.parse_list(event_object.late_evening_time)
 
 
         duration = dynamic_event_duration + buffer
 
-        for i in range (0, len(User1.free_time)):
-            if User1.free_time[i][1] > duration:
-                print 'Can place at: ', User1.free_time[i][1]
+        for i in range (0, len(event_object.free_time)):
+            if event_object.free_time[i][1] > duration:
+                print 'Can place at: ', event_object.free_time[i][1]
